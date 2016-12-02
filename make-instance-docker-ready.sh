25097d4da35da6e76b9a37d454c506a09ab042a7
Joachim Eckert <eckert@eisit.de>
2016-12-02 09:13:28 +0100

    removed sudo

--- make-instance-docker-ready.sh
+++ make-instance-docker-ready.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo yum update -y
-sudo yum install -y docker
-sudo service docker start
-sudo usermod -a -G docker ec2-user
+yum update -y
+yum install -y docker
+service docker start
+usermod -a -G docker ec2-user
 
